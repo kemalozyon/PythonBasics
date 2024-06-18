b934732f464139131ed50c766db8ab5b7c3fea1b
Kemal Özyön <kemalozyon51@gmail.com>
2024-06-19 00:31:23 +0300

    Variables Complited

---
+++ Variables.py
@@ -0,0 +1,21 @@
+#İnteger
+a = 3
+b = 4
+c = a + b
+print(c)
+
+#float
+
+x = 3.5
+y = 4.567
+z= x + y
+print(z)
+
+#Variables
+
+x = 3
+y = 5
+
+print(x**y) # x üssü y
+print(x*(int(input("Bir sayı girin: "))))
+print(type(x + y))
