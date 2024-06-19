21f8ae46fd4af6ab36c316e038a7a5988d671dc3
Kemal Özyön <kemalozyon51@gmail.com>
2024-06-19 03:37:53 +0300

    String finished

---
+++ Strings.py
@@ -0,0 +1,19 @@
+x = "Wanting to be a pilot"
+
+print(type(x))
+print(len(x))
+print(f"{x}, \nis what you want")
+print(f"hello \t {x}")
+
+
+#String Advanced
+
+my_string = "hello world"
+
+#Indexing
+print(my_string[0])
+#ASCII code:
+print(ord(my_string[0]))
+print(chr(ord(my_string[2])))
+
+
