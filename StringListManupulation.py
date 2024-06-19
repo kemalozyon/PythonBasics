16919f2239c842db413cd3925439a0e2f23e35c5
Kemal Özyön <kemalozyon51@gmail.com>
2024-06-19 13:51:49 +0300

    String Manupulation done

---
+++ StringListManupulation.py
@@ -0,0 +1,19 @@
+#Indexing:
+myString = "Kemal Ozyon"
+for i in range(len(myString)):
+    print(myString[i])
+
+#Slicing:
+
+myString2 = "123456789"
+print(myString2[1::2]) # [first index(included):last index(unincluded):step size]
+
+#StringMethods:
+
+my_name = "kemal"
+print(my_name.capitalize())
+myList = list(myString.split())
+print(myList)
+for i in range(len(myList)):
+    print(myList[i])
+
